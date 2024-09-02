646985a278709829210e7eb614ac195fb53408ef
FizzleOut <fizzleout1337@gmail.com>
2024-09-02 11:51:28 +0200

    Underlay component

---
+++ src/components/Base/AppUnderlay.vue
@@ -0,0 +1,22 @@
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AppUnderlay',
+
+});
+</script>
+
+<template>
+  <div class="underlay">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+  .underlay {
+    border-radius: 50%;
+    backdrop-filter: blur(2px);
+    background: rgba(1, 26, 56, 0.8);
+  }
+</style>
