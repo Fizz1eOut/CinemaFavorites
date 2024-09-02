81dd58feb7d26dd0afe87cc3a17c4c6b7150b176
FizzleOut <fizzleout1337@gmail.com>
2024-09-02 11:50:16 +0200

    Button component

---
+++ src/components/Base/AppButton.vue
@@ -0,0 +1,37 @@
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AppButton',
+
+});
+</script>
+
+<template>
+  <button type="button" class="button">
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+.button {
+  padding: 12px 20px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: 400;
+  font-size: 17px;
+  letter-spacing: 0.01em;
+  font-family: "Open Sans", sans-serif;
+  color: var(--color-black);
+  background-color: var(--color-yellow);
+  border-radius: 10px;
+  transition: background-color 0.3s ease-in-out;
+  cursor: pointer;
+}
+.button:hover {
+  background-color: var(--color-yellow-hover);
+}
+</style>
