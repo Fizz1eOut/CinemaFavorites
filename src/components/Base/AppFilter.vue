5f3e2745d17ca742402acf8ee04a38ee5595a49b
FizzleOut <fizzleout1337@gmail.com>
2024-10-10 19:56:47 +0200

    The AppFilter component has been created, supporting adaptive display of filters for mobile and desktop devices. The logic for tracking a media query has been implemented to change the display state (isMobile) depending on the screen width (800px). Subscription and unsubscription from media query change events occurs when the component is mounted and unmounted.

--- src/components/Base/AppFilter.vue
+++ src/components/Base/AppFilter.vue
@@ -1,44 +1,116 @@
 <script>
 import { defineComponent } from 'vue';
+import AppButton from '@/components/Base/AppButton.vue';
+import IconFilter from '@/components/Icons/IconFilter.vue';
+
+const mediaQueryList = window.matchMedia('(max-width: 800px)');
 
 export default defineComponent({
-  name: 'AppMovieCard',
+  name: 'AppFilter',
 
-  props: {
-
+  components: {
+    AppButton,
+    IconFilter,
   },
 
-  // data() {
-  //   return {
-  //   };
-  // },
+  data() {
+    return {
+      active: false,
+      isMobile: false,
+    };
+  },
 
-  // computed: {
-  // },
-  
-  // methods: {
-  
-  // }
+  mounted() {
+    // Подписываемся на событие изменения медиазапроса при монтировании компонента
+    this.handleMediaChange(mediaQueryList); // Инициализация значения isMobile при монтировании компонента
+    mediaQueryList.addEventListener('change', this.handleMediaChange);
+  },
+
+  beforeUnmount() {
+    // Отписываемся от события изменения медиазапроса при размонтировании компонента
+    mediaQueryList.removeEventListener('change', this.handleMediaChange);
+  },
+
+  methods: {
+    toggleFilter() {
+      this.active = !this.active;
+    },
+    // Обработчик изменения состояния медиазапроса
+    handleMediaChange(event) {
+      // Обновляем флаг isMobile в соответствии с состоянием медиазапроса
+      this.isMobile = event.matches;
+    }
+  }
 });
 </script>
 
 <template>
-  <div class="filter">
+  <div v-if="!isMobile" class="filter">
     <div class="filter__items">
       <slot />
     </div>
   </div>
+
+  <div v-if="isMobile" class="filter-mobile">
+    <app-button 
+      default
+      class="filter-mobile__button"
+      @click="toggleFilter"
+    >
+      <icon-filter class="icon-filter" />
+      <h3 class="filter-mobile__title">Фільтри</h3>
+    </app-button>
+
+
+    <Transition>
+      <div v-if="active" class="filter-mobile__body">
+        <div class="filter-mobile__items">  
+          <slot class="filter-mobile__item" />
+        </div>
+      </div>
+    </Transition>
+  </div>
 </template>
 
 <style scoped>
-.filter {
-}
-.filter__items {
-  display: flex;
-  align-items: flex-start;
-  justify-content: space-between;
-  gap: 10px;
-}
-.filter__item {
-}
+  .filter {
+    margin-top: 40px;
+  }
+  .filter__items {
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+    gap: 10px;
+  }
+
+  .filter-mobile {
+    margin-top: 40px;
+  }
+  .icon-filter {
+    width: 22px;
+    height: 22px;
+    fill: var(--color-light-blue);
+  }
+  .filter-mobile__title {
+    font-weight: 400;
+    font-size: 17px;
+    color: var(--color-light-blue);
+    font-family: "Open Sans", sans-serif;
+  }
+  .filter-mobile__body {
+    margin-top: 20px;
+  }
+  .filter-mobile__items> *:not(:last-child) {
+    margin-bottom: 10px;
+  }
+
+  .v-enter-active,
+  .v-leave-active {
+    transition: opacity 0.7s ease;
+  }
+
+  .v-enter-from,
+  .v-leave-to {
+    opacity: 0;
+  }
 </style>
