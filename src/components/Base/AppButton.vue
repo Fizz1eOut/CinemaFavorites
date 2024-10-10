b4fc77acafafaf253a60a6739436513a5590e8ab
FizzleOut <fizzleout1337@gmail.com>
2024-10-10 19:50:35 +0200

    The AppButton component has been created with the ability to select button styles via props. Three style types are supported: yellow, blue, and the default button with the ability to switch via the Boolean props yellow, blue, and default. A system of dynamic classes has been implemented based on the transferred props.

--- src/components/Base/AppButton.vue
+++ src/components/Base/AppButton.vue
@@ -4,44 +4,94 @@ import { defineComponent } from 'vue';
 export default defineComponent({
   name: 'AppButton',
 
+  props: {
+    yellow: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    blue: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    default: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+  },
+  
+  computed: {
+    classes() {
+      return {
+        'button-yellow': this.yellow,
+        'button-blue': this.blue,
+        'button-default': this.default,
+      };
+    }
+  }
+
 });
 </script>
 
 <template>
-  <button type="button" class="button-default">
+  <button type="button" class="button" :class="classes">
     <slot />
   </button>
 </template>
 
 <style scoped>
-.button {
-  padding: 12px 20px;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-weight: 400;
-  font-size: 17px;
-  letter-spacing: 0.01em;
-  font-family: "Open Sans", sans-serif;
-  color: var(--color-black);
-  background-color: var(--color-yellow);
-  border-radius: 10px;
-  transition: background-color 0.3s ease-in-out;
-  cursor: pointer;
-}
-.button:hover {
-  background-color: var(--color-yellow-hover);
-}
+  .button-yellow {
+    padding: 12px 20px;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-weight: 400;
+    font-size: 17px;
+    letter-spacing: 0.01em;
+    font-family: "Open Sans", sans-serif;
+    color: var(--color-black);
+    background-color: var(--color-yellow);
+    border-radius: 10px;
+    transition: background-color 0.3s ease-in-out;
+    cursor: pointer;
+  }
+  .button-yellow:hover {
+    background-color: var(--color-yellow-hover);
+  }
 
-.button-default {
-  display: flex;
-  gap: 8px;
-  padding: 0;
-  border-radius: 0;
-  color: var(--color-white);
-  background-color: var(--color-background);
-  cursor: pointer;
-}
+  .button-blue {
+    padding: 12px 20px;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-weight: 400;
+    font-size: 17px;
+    letter-spacing: 0.01em;
+    font-family: "Open Sans", sans-serif;
+    color: var(--color-black);
+    background-color: var(--color-light-blue);
+    border-radius: 10px;
+    transition: background-color 0.3s ease-in-out;
+    cursor: pointer;
+  }
+  .button-blue:hover {
+    color: var(--color-light-blue-hover);
+  }
+
+  .button-default {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    padding: 0;
+    border-radius: 0;
+    color: var(--color-white);
+    background-color: var(--color-background);
+    cursor: pointer;
+  }
 </style>
